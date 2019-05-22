d0618d1207e5e99b319e16893aaae895c978cd8a
Andrei VOICU <andrei.voicu@nstel.fr>
2019-05-22 21:12:14 +0200

    nav et design

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -35,6 +35,64 @@ export class NavbarComponent implements OnInit {
         this.isNavbarCollapsed = true;
     }
 
+    loadJS() {
+        (function($) {
+            'use strict'; // Start of use strict
+
+            // Toggle the side navigation
+            $('#sidebarToggle, #sidebarToggleTop').on('click', function(e) {
+                $('body').toggleClass('sidebar-toggled');
+                $('.sidebar').toggleClass('toggled');
+                if ($('.sidebar').hasClass('toggled')) {
+                    $('.sidebar .collapse').collapse('hide');
+                }
+            });
+
+            // Close any open menu accordions when window is resized below 768px
+            $(window).resize(function() {
+                if ($(window).width() < 768) {
+                    $('.sidebar .collapse').collapse('hide');
+                }
+            });
+
+            // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+            $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+                if ($(window).width() > 768) {
+                    const e0 = e.originalEvent,
+                        delta = e0.wheelDelta || -e0.detail;
+                    this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+                    e.preventDefault();
+                }
+            });
+
+            // Scroll to top button appear
+            $(document).on('scroll', function() {
+                const scrollDistance = $(this).scrollTop();
+                if (scrollDistance > 100) {
+                    $('.scroll-to-top').fadeIn();
+                } else {
+                    $('.scroll-to-top').fadeOut();
+                }
+            });
+
+            // Smooth scrolling using jQuery easing
+            $(document).on('click', 'a.scroll-to-top', function(e) {
+                const $anchor = $(this);
+                $('html, body')
+                    .stop()
+                    .animate(
+                        {
+                            scrollTop: $($anchor.attr('href')).offset().top
+                        },
+                        1000,
+                        'easeInOutExpo'
+                    );
+                e.preventDefault();
+            });
+            // @ts-ignore
+        })(jQuery); // End of use strict
+    }
+
     ngOnInit() {
         this.languageHelper.getAll().then(languages => {
             this.languages = languages;
@@ -44,6 +102,8 @@ export class NavbarComponent implements OnInit {
             this.inProduction = profileInfo.inProduction;
             this.swaggerEnabled = profileInfo.swaggerEnabled;
         });
+
+        this.loadJS();
     }
 
     changeLanguage(languageKey: string) {
