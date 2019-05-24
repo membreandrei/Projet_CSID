2790401df36d39aa7b3ca128e911bc4e5994bf97
Andrei VOICU <andrei.voicu@nstel.fr>
2019-05-24 13:27:13 +0200

    grahp primeng

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -2,9 +2,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProjetCsidSharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartModule } from 'primeng/chart';
 
 @NgModule({
-    imports: [ProjetCsidSharedModule, RouterModule.forChild([HOME_ROUTE])],
+    imports: [ProjetCsidSharedModule, BrowserAnimationsModule, ChartModule, RouterModule.forChild([HOME_ROUTE])],
     declarations: [HomeComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
