b866d075718054d9ad6003bb3208746869433582
Andrei VOICU <andrei.voicu@nstel.fr>
2019-06-14 09:33:02 +0200

    'test

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -49,48 +49,6 @@ export class NavbarComponent implements OnInit {
                 }
             });
 
-            // Close any open menu accordions when window is resized below 768px
-            $(window).resize(function() {
-                if ($(window).width() < 768) {
-                    $('.sidebar .collapse').collapse('hide');
-                }
-            });
-
-            // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-            $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
-                if ($(window).width() > 768) {
-                    const e0 = e.originalEvent,
-                        delta = e0.wheelDelta || -e0.detail;
-                    this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-                    e.preventDefault();
-                }
-            });
-
-            // Scroll to top button appear
-            $(document).on('scroll', function() {
-                const scrollDistance = $(this).scrollTop();
-                if (scrollDistance > 100) {
-                    $('.scroll-to-top').fadeIn();
-                } else {
-                    $('.scroll-to-top').fadeOut();
-                }
-            });
-
-            // Smooth scrolling using jQuery easing
-            $(document).on('click', 'a.scroll-to-top', function(e) {
-                const $anchor = $(this);
-                $('html, body')
-                    .stop()
-                    .animate(
-                        {
-                            scrollTop: $($anchor.attr('href')).offset().top
-                        },
-                        1000,
-                        'easeInOutExpo'
-                    );
-                e.preventDefault();
-            });
-
             $(document).ready(function() {
                 $('#sidebarToggle').click(function() {
                     $('#sidebarToggle').toggleClass('rotated');
